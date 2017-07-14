3e0207cf73b1b09ab54397ef1e12f6cd1c885d00
JeongHaewon <onlysun217@gmail.com>
2017-07-13 18:38:27 -0700

    syntax error near unexpected token

--- whois.py
+++ whois.py
@@ -4,17 +4,19 @@ import argparse # add args parsing
 import json # json
 import pythonwhois
 
-data = pythonwhois.net.get_whois_raw("goodle.com", with_server_list=False)
-print data[0]
+data = pythonwhois.net.get_whois_raw( 'http://google.com', with_server_list=False)
+print DATA[0]
 
 parser = argparse.ArgumentParser(description='Hi')
 parser .add_argument('--Domain', action='store', dtest='Domain')
 
 output = parser.parse_args[data] #read domains from a file
-print output
-
+print (output)
 
 
 whois_json = json.dumps(data) #parse whois output (json)
 data2 = json.loads(whois_json)
+
 fileobj = open("whois_output", 'wt') #store output as a file
+fileobj.write(data2)
+fileobj.close()
