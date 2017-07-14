d9593c8d4a1c5596573c1a1c00d72b719dd44167
JeongHaewon <onlysun217@gmail.com>
2017-07-13 17:21:48 -0700

    update2

--- whois.py
+++ whois.py
@@ -3,3 +3,16 @@ import pythonwhois
 data = pythonwhois.net.get_whois_raw("goodle.com", with_server_list=False)
 
 print data[0]
+
+import argparse # add args parsing
+
+print data[Domain name] #read domains from a file
+
+
+import json # json
+
+whois_json = json.dumps(data) #parse whois output (json)
+
+data2 = json.loads(whois_json)
+fileobj = open ("whois_output",'wt') #store output as a file
+
